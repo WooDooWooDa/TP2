7ba20a0fab5842c46c98ecac9f33ea41ccbf7014
WooDooWooDa <jeremie-bouchard@hotmail.fr>
2020-10-21 15:55:09 -0400

    remove one method useless

--- src/tp/Line.java
+++ src/tp/Line.java
@@ -18,10 +18,6 @@ public class Line {
         return binaryString;
     }
 
-    public String getBinaryStringWithoutParity() {
-        return binaryString.substring(0, 8);
-    }
-
     public String getStringChar() {
         return String.valueOf(stringChar);
     }
